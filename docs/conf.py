06094cabd87835957a032ec83bebae0899c3c9eb
Meisam Taghizadeh <meisam2236@gmail.com>
2025-04-06 01:36:19 +0330

    fix some issues

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
+    'myst_parser'
+]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
---
+++ docs/index.rst
@@ -0,0 +1,8 @@
+Welcome to ViSERA
+===========================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   index.md
